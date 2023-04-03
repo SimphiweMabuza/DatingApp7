54f4d1b5f1cb25380f4d1864049dbaf2bafe986d
SimphiweMabuza <2016363729@ufs4life.ac.za>
2023-04-03 20:17:50 +0200

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
-
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
@@ -24,7 +24,6 @@ export class AccountService {
       })
     )
   }
-
   register(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
@@ -35,13 +34,11 @@ export class AccountService {
       })
     )
   } 
-
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
-
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
-}
+} 
